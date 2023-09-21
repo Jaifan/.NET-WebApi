b4dc108e89b3c5e24923f94756d45c5825ccf934
Jaifan <jiku1213@gmail.com>
2023-09-21 18:09:05 +0400

    implement service response for api

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -4,22 +4,28 @@ namespace test.Services.CharacterService
     public class CharacterService : ICharacterService
     {
         private static List<Character> characters = new List<Character> {
-            new Character(), new Character{ Id= 1 ,Name ="Zakaria"}
+            new Character(), new Character{ Id= 1 ,Name ="ABC"}
         };
-        public async Task<List<Character>> AddCharacter(Character newCharacter)
+        public async Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter)
         {
+            var serviceResponse = new ServiceResponse<List<Character>>();
             characters.Add(newCharacter);
-            return characters;
+            serviceResponse.Data = characters;
+            return serviceResponse;
         }
 
-        public async Task<List<Character>> GetAllCharacters()
+        public async Task<ServiceResponse<List<Character>>> GetAllCharacters()
         {
-            return characters;
+            var serviceResponse = new ServiceResponse<List<Character>>();
+            serviceResponse.Data = characters;
+            return serviceResponse;
         }
 
-        public async Task<Character> GetCharacterById(int id)
+        public async Task<ServiceResponse<Character>> GetCharacterById(int id)
         {
-            return characters[0];
+            var serviceResponse = new ServiceResponse<Character>();
+            serviceResponse.Data = characters.FirstOrDefault(c=>c.Id == id);
+            return serviceResponse;
         }
     }
 }
