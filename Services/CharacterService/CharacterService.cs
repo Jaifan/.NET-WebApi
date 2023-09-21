99e69a2f4ca2987708bb862b1657160e7ee7e1ca
Jaifan <jiku1213@gmail.com>
2023-09-21 14:31:00 +0400

    Implement async await system

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -6,18 +6,18 @@ namespace test.Services.CharacterService
         private static List<Character> characters = new List<Character> {
             new Character(), new Character{ Id= 1 ,Name ="Zakaria"}
         };
-        public List<Character> AddCharacter(Character newCharacter)
+        public async Task<List<Character>> AddCharacter(Character newCharacter)
         {
             characters.Add(newCharacter);
             return characters;
         }
 
-        public List<Character> GetAllCharacters()
+        public async Task<List<Character>> GetAllCharacters()
         {
             return characters;
         }
 
-        public Character GetCharacterById(int id)
+        public async Task<Character> GetCharacterById(int id)
         {
             return characters[0];
         }
--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -2,9 +2,9 @@ namespace test.Services.CharacterService
 {
     public interface ICharacterService
     {
-         List<Character> GetAllCharacters();
-         Character GetCharacterById(int id);
-         List<Character> AddCharacter(Character newCharacter);
+         Task<List<Character>> GetAllCharacters();
+         Task<Character> GetCharacterById(int id);
+         Task<List<Character>> AddCharacter(Character newCharacter);
 
     }
 }
